d79053906df560c0f0c01b0c32752a9a184c3834
leilamgomes@hotmail.com <leilamgomes@hotmail.com>
2022-12-14 00:26:15 +0000

    Aprendendo SELECT * FROM

---
+++ dia01/Select.sql
@@ -0,0 +1,14 @@
+-- Databricks notebook source
+SELECT 'OLÁ MUNDO'
+
+-- COMMAND ----------
+
+SELECT 1 + 10 * 2
+
+-- COMMAND ----------
+
+SELECT ABS(10 - 1000)
+
+-- COMMAND ----------
+
+
---
+++ dia01/from.sql
@@ -0,0 +1,44 @@
+-- Databricks notebook source
+SELECT
+  *
+FROM
+  silver_olist.pedido 
+
+-- selecione TUDO do banco de dados silver_olist e tabela pedido
+
+-- COMMAND ----------
+
+SELECT
+  idPedido,
+  descSituacao
+FROM
+  silver_olist.pedido
+
+-- Selecione a coluna idPedido e descSituacao da tabela pedido
+
+-- COMMAND ----------
+
+SELECT
+  idPedido,
+  descSituacao
+FROM
+  silver_olist.pedido
+LIMIT
+  5
+
+-- Selecione a coluna idPedido e descSituacao da tabela pedido com limite de 5 linhas
+
+-- COMMAND ----------
+
+SELECT
+  *,
+  DATEDIFF(dtEstimativaEntrega, dtEntregue) AS qtDiasPromessaEntrega
+FROM
+  silver_olist.pedido
+  
+-- DATEDIFF é uma função do SQL/Spark para calcular a diferença de dias entre duas datas.
+-- AS alias, apelido para a nova coluna. O AS é opcional, porém fica mais legível.
+
+-- COMMAND ----------
+
+
